14fdcd998f8f46c26cc033b751d1d05d705204e0
dhiab <dhiababdallah1@gmail.com>
2026-02-18 19:56:22 +0100

    task 1.2.3.4 end 5 ok

--- app/src/main/java/com/ElOuedUniv/maktaba/data/repository/BookRepository.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/data/repository/BookRepository.kt
@@ -9,7 +9,7 @@ import com.ElOuedUniv.maktaba.data.model.Book
 class BookRepository {
 
     /**
-     * TODO for Students (TP1 - Exercise 1):
+     *
      * Complete the book information for each book in the list below.
      * Add the following information for each book:
      * - isbn: Use a valid ISBN-13 format (e.g., "978-3-16-148410-0")
@@ -23,15 +23,51 @@ class BookRepository {
      * )
      */
     private val booksList = listOf(
+
+        // ----- Previous 5 books -----
+
+        Book(
+            isbn = "978-0-262-03384-8",
+            title = "Introduction to Algorithms",
+            nbPages = 1312
+        ),
+
+        Book(
+            isbn = "978-0-596-00712-6",
+            title = "Head First Design Patterns",
+            nbPages = 694
+        ),
+
+        Book(
+            isbn = "978-1-449-37464-8",
+            title = "Learning SQL",
+            nbPages = 338
+        ),
+
+        Book(
+            isbn = "978-1-491-92424-9",
+            title = "Android Programming with Kotlin",
+            nbPages = 450
+        ),
+
+        Book(
+            isbn = "978-1-59327-599-0",
+            title = "Automate the Boring Stuff with Python",
+            nbPages = 504
+        )
+    )
+
+    private val booksLit = listOf(
         Book(isbn = "", title = "Clean Code", nbPages = 0),
         Book(isbn = "", title = "The Pragmatic Programmer", nbPages = 0),
         Book(isbn = "", title = "Design Patterns", nbPages = 0),
         Book(isbn = "", title = "Refactoring", nbPages = 0),
         Book(isbn = "", title = "Head First Design Patterns", nbPages = 0)
+
     )
 
     /**
-     * TODO for Students (TP1 - Exercise 2):
+     *
      * Add 5 more books to the list above.
      * Choose books related to Computer Science, Programming, or any topic you like.
      * Remember to include complete information (ISBN, title, nbPages).
@@ -46,6 +82,8 @@ class BookRepository {
      * Get all books from the repository
      * @return List of all books
      */
+
+
     fun getAllBooks(): List<Book> {
         return booksList
     }
@@ -58,4 +96,5 @@ class BookRepository {
     fun getBookByIsbn(isbn: String): Book? {
         return booksList.find { it.isbn == isbn }
     }
+}
 }
