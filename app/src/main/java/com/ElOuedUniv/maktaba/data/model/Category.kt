7f97ffe68a76ce069889972289ac648b711073f5
dhiab <dhiababdallah1@gmail.com>
2026-03-01 15:32:02 +0100

    TP2: 1.2.3.4.5.6 END 7 OK

--- app/src/main/java/com/ElOuedUniv/maktaba/data/model/Category.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/data/model/Category.kt
@@ -2,7 +2,7 @@ package com.ElOuedUniv.maktaba.data.model
 
 // TODO: Complete the Category data class implementation
 data class Category(
-        val id: String,
-        val name: String,
-        val description: String
-    )
+    val id: String,
+    val name: String,
+    val description: String
+)
